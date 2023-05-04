3677304e4e1f05aa8e1c2fd9c20c5ecfe83a0444
IamVDD <vodinhdat03@gmail.com>
2023-05-04 21:24:52 +0700

    fix and optimize

--- src/fit_model.py
+++ src/fit_model.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def DataLoader(X_train,y_train,batch_size=64):
     """Split data into batch"""
@@ -29,9 +31,24 @@ def FitModel(X_train,X_test,y_train,y_test,model,criterion,optimizer,epoch,batch
             y_test_pred  = model.forward(X_test)
             train_acc = torch.mean((torch.argmax(y_train_pred,dim=1)==torch.argmax(y_train,dim=1)) * 1.0)
             val_acc   = torch.mean((torch.argmax(y_test_pred,dim=1)==torch.argmax(y_test,dim=1)) * 1.0)
-            print(f'EPOCH {e:>5} | LOSS: {total_loss:.4f} | TRAIN ACC: {train_acc* 100:.2f}% | VAL ACC: {val_acc*100:.2f}% |')
+            train_acc_list.append(train_acc*100)
+            val_acc_list.append(val_acc*100)
+            print(f'EPOCH {e:>5} | LOSS: {total_loss:.4f} | TRAIN ACC: {train_acc* 100:.2f}% | TEST ACC: {val_acc*100:.2f}% |')
     
     Plot(train_acc_list,val_acc_list)
 
 def Plot(train_acc_list,train_val_list):
-    pass
+    sns.set_style('darkgrid') # darkgrid, white grid, dark, white and ticks
+    plt.rc('axes', titlesize=18)     # fontsize of the axes title
+    plt.rc('axes', labelsize=14)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=13)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=13)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=13)    # legend fontsize
+    plt.rc('font', size=13)
+    plt.figure(figsize=(12,6))
+    plt.plot(train_acc_list)
+    plt.plot(train_val_list)
+    plt.legend(['Train','Test'])
+    plt.xlabel('Epoch')
+    plt.ylabel('%')
+    plt.show()
--- src/split_data.py
+++ src/split_data.py
@@ -1,23 +1,25 @@
 import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler,OneHotEncoder
-from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+from imblearn.over_sampling import RandomOverSampler, SMOTE
 def SplitData(df):
     numpy_df = np.array(df)
     X = numpy_df[:,:-1]
     y = numpy_df[:,-1]
-    ros = RandomOverSampler()
-    X,y = ros.fit_resample(X,y)
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
+    # ros = SMOTE()
+    # X,y = ros.fit_resample(X,y)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0,stratify=y)
 
-    scaler  = MinMaxScaler()
+    scaler  = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test  = scaler.transform(X_test)
 
     one_hot = OneHotEncoder()
     y_train = one_hot.fit_transform(y_train.reshape(-1,1)).toarray()
     y_test  = one_hot.transform(y_test.reshape(-1,1)).toarray()
+
+    # X_train,y_train = ros.fit_resample(X_train,y_train)
 
     X_train = torch.tensor(X_train,dtype=torch.float32)
     X_test  = torch.tensor(X_test,dtype=torch.float32)
